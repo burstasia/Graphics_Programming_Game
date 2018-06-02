21c974f63c3d3a7bc3154e793359a229833536d4
nicole_munro <burstasia@gmail.com>
2018-06-02 17:40:12 +0200

    Added diamond mesh, and started a trigger callback thingu

--- GP2_Munro_Nicole_Game/OverlordProject/CourseObjects/Game/Level.cpp
+++ GP2_Munro_Nicole_Game/OverlordProject/CourseObjects/Game/Level.cpp
@@ -21,8 +21,18 @@ Level::~Level()
 
 void Level::Initialize(const GameContext & gameContext)
 {
+	auto pColorMaterial = new ColorMaterial();
+	pColorMaterial->SetColor(XMFLOAT4(0.0f, 0.5f, 0.5f, 1.0f));
+
+	gameContext.pMaterialManager->AddMaterial(pColorMaterial, UINT(0));
 	UNREFERENCED_PARAMETER(gameContext);
 
+	//InitLevel(gameContext);
+	InitItems(gameContext);
+}
+
+void Level::InitLevel(const GameContext & gameContext)
+{
 	auto physX = PhysxManager::GetInstance()->GetPhysics();
 	auto bouncyMaterial = physX->createMaterial(0.5, 0.5, 1.0f);
 
@@ -39,11 +49,40 @@ void Level::Initialize(const GameContext & gameContext)
 
 	AddComponent(pGroundModel);
 
-	auto pColorMaterial = new ColorMaterial();
-	pColorMaterial->SetColor(XMFLOAT4(0.0f, 0.5f, 0.5f, 1.0f));
-
-	gameContext.pMaterialManager->AddMaterial(pColorMaterial, UINT(0));
-
 	pGroundModel->SetMaterial(0);
+}
+
+void Level::InitItems(const GameContext & gameContext)
+{
+	auto physX = PhysxManager::GetInstance()->GetPhysics();
+	auto bouncyMaterial = physX->createMaterial(0.5, 0.5, 1.0f);
+
+	GameObject *diamond = new GameObject();
+
+	auto diamondModel = new ModelComponent(L"Resources/Meshes/diamond.ovm");
+
+	diamond->AddComponent(diamondModel);
+
+	diamond->AddComponent(new RigidBodyComponent(true));
+
+	diamondModel->SetMaterial(0);
+
+	diamond->SetOnTriggerCallBack(test);
+
+	std::shared_ptr<PxGeometry> geometry(new PxBoxGeometry(1.0f, 1.0f, 1.0f));
+
+	ColliderComponent *collider = new ColliderComponent(geometry, *bouncyMaterial);
+
+	collider->EnableTrigger(true);
+
+	diamond->AddComponent(collider);
+
+	AddChild(diamond);
+}
+
+void Level::test(GameObject * triggerobject, GameObject * otherobject, TriggerAction action)
+{
+
+	Logger::LogInfo(L"EYO YOU HIT A DIAMOND NEATO WOW");
 
 }
--- GP2_Munro_Nicole_Game/OverlordProject/CourseObjects/Game/Level.h
+++ GP2_Munro_Nicole_Game/OverlordProject/CourseObjects/Game/Level.h
@@ -8,5 +8,13 @@ public:
 
 	void Initialize(const GameContext & gameContext);
 
+
+private:
+
+	void InitLevel(const GameContext & gameContext);
+	void InitItems(const GameContext & gameContext);
+
+	static void test(GameObject* triggerobject, GameObject* otherobject, TriggerAction action);
+
 };
 
